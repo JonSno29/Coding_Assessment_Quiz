934ff6cc2dd95a1fe3e19bfd26cfa6aef057b6af
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-09-23 20:49:50 -0700

    Added variables to JavaScript 1-22

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Variables */
+var startQuizBtn = document.querySelector("#start-button");
+var timer = document.querySelector(".timer");
+var highScoreBtn = document.querySelector("#highscore-button");
+var question = document.querySelector("#question");
+var assessQuestions = document.querySelector("#assess-questions");
+var multipleChoice = document.querySelector("#multiple-choice");
+var answer = document.querySelector("#answer");
+var multipleChoice = document.querySelector("#multiple-choice");
+var choiceA = document.querySelector("#multiple-choice-A");
+var choiceB = document.querySelector("#multiple-choice-B");
+var choiceC = document.querySelector("#multiple-choice-C");
+var choiceD = document.querySelector("#multiple-choice-D");
+var checkAnswerIncorrect = document.querySelector(".check-answer-incorrect");
+var checkAnswerCorrect = document.querySelector("#check-answer-correct");
+var scoreBtn = document.querySelector("#score-button");
+var inputScore = document.querySelector("#input-score");
+var initialsBox = document.querySelector("#initials-box");
+var highScore = document.querySelector("#high-score");
+var backBtn = document.querySelector("#back-button");
+var submitBtn = document.querySelector("#submit-button");
+var clearBtn = document.querySelector("#clear-button");
+var start = document.querySelector(".start");
+/* Fin Variables*/
